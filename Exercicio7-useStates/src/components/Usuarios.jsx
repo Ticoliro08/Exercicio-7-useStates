c57c86c7e5e951e00624bb2a7b8d6ff3619855f9
Guih <ggsantana18@gmail.com>
2025-02-19 14:41:57 -0300

    12

--- Exercicio7-useStates/src/components/Usuarios.jsx
+++ Exercicio7-useStates/src/components/Usuarios.jsx
@@ -7,6 +7,7 @@ function perfil(props) {
      <nav className="bloco">
 
     
+    
           <h2>{props.nome}</h2>
           <img className="imagem" src={props.img} />
           <h3>Hobby:<p>{props.hobby}</p></h3>
